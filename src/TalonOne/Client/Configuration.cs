8e434e6d978f2a7f5254700e49d66fac31bf4ed1
Jakob (Koby) Shimony <shimony@talon.one>
2022-03-27 10:29:51 +0200

    bump version

--- src/TalonOne/Client/Configuration.cs
+++ src/TalonOne/Client/Configuration.cs
@@ -31,7 +31,7 @@ namespace TalonOne.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "4.0.3";
+        public const string Version = "4.0.4";
 
         /// <summary>
         /// Identifier for ISO 8601 DateTime Format
@@ -96,7 +96,7 @@ namespace TalonOne.Client
         [System.Diagnostics.CodeAnalysis.SuppressMessage("ReSharper", "VirtualMemberCallInConstructor")]
         public Configuration()
         {
-            UserAgent = "OpenAPI-Generator/4.0.3/csharp";
+            UserAgent = "OpenAPI-Generator/4.0.4/csharp";
             BasePath = "http://localhost";
             DefaultHeaders = new ConcurrentDictionary<string, string>();
             ApiKey = new ConcurrentDictionary<string, string>();
@@ -351,7 +351,7 @@ namespace TalonOne.Client
             String report = "C# SDK (TalonOne) Debug Report:\n";
             report += "    OS: " + System.Runtime.InteropServices.RuntimeInformation.OSDescription + "\n";
             report += "    Version of the API: 1.0.0\n";
-            report += "    SDK Package Version: 4.0.3\n";
+            report += "    SDK Package Version: 4.0.4\n";
 
             return report;
         }
