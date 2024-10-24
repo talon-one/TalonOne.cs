7a1ee92a6bc8a08a5c5292f1570e3c2a9176be6d
rubyjohn93 <john@talon.one>
2024-10-24 09:54:58 +0200

    set EmitDefaultValue=true for price, usageLimit and position

--- src/TalonOne/Model/StrikethroughChangedItem.cs
+++ src/TalonOne/Model/StrikethroughChangedItem.cs
@@ -90,7 +90,7 @@ namespace TalonOne.Model
         /// The price of the changed item.
         /// </summary>
         /// <value>The price of the changed item.</value>
-        [DataMember(Name="price", EmitDefaultValue=false)]
+        [DataMember(Name="price", EmitDefaultValue=true)]
         public decimal Price { get; set; }
 
         /// <summary>
